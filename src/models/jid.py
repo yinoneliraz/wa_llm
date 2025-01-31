38b0733284bb6e72d36c04a87b469efa7659feea
almogbaku <almog.baku@gmail.com>
2025-01-31 10:08:40 +0200

    fix: wrong jid parse?

--- src/models/jid.py
+++ src/models/jid.py
@@ -79,7 +79,7 @@ def parse_ad_jid(user: str) -> JID:
 def parse_jid(jid: str) -> JID:
     parts = jid.split("@")
     if len(parts) == 1:
-        return new_jid("", parts[0])
+        return new_jid(parts[0], DefaultUserServer)
     elif ":" in parts[0]:
         if parts[1] == DefaultUserServer:
             if "." in parts[0]:
