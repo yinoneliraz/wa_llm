822eea1266ff2071f2d16f6ad02a83fc8b16c94d
almogbaku <almog.baku@gmail.com>
2025-02-19 12:36:43 +0200

    try to fix ingest

--- src/daily_ingest/daily_ingest.py
+++ src/daily_ingest/daily_ingest.py
@@ -165,8 +165,6 @@ class topicsLoader:
     async def load_topics_for_all_groups(
         self, session: AsyncSession, embedding_client: AsyncClient
     ):
-        groups =  await session.exec(select(Group))
-        for group in groups:
-            if not group.managed:
-                continue
+        groups = await session.exec(select(Group).where(Group.managed is True))
+        async for group in groups:
             await self.load_topics(session, group.group_jid, embedding_client)
